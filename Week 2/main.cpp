34edd2c25282d1b6195dd466d4f9440aec270d8b
saltmurai <skytracker12345@gmail.com>
2021-03-01 10:06:21 +0700

    inital commit

--- Week 2/main.cpp
+++ Week 2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-using namespace std;
 #define MAX 10
+using namespace std;
+
 template <class T>
 class Stack
 {
@@ -33,7 +34,7 @@ public:
     T pop()
     {
         if (this->is_empty())
-            return 0;
+            exit(0);
         else
         {
             this->n--;
@@ -43,20 +44,69 @@ public:
     T top()
     {
         if (this->is_empty())
-            return NULL;
+            exit(0);
         else
             return this->info[this->n - 1];
     }
 };
 
-int main() { 
+template <class T>
+class Queue
+{
+    Stack<T> s1, s2;
+
+public:
+    Queue() {
+        s1.initialize();
+        s2.initialize();
+    }
+    void en_queue(T K)
+    {
+        // Di chuyen cac phan tu tu s1 sang s2
+        while (!s1.is_empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        // Push vao s1
+        s1.push(K);
+        //Di chuyen lai s2 vao s1
+        while (!s2.is_empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    T de_queue()
+    {
+        if (s1.is_empty())
+        {
+            cout << "Queue is empty";
+            exit(0);
+        }
+
+        T K = s1.top();
+        s1.pop();
+        return K;
+    }
+};
+int main()
+{
     Stack<int> stack1;
     stack1.initialize();
     stack1.push(3);
     stack1.push(4);
     stack1.push(5);
     stack1.push(6);
-    
-    for(int i = 0; i < 4; ++ i) 
-    cout << stack1.pop() << endl;
+    cout << "Stack test: " << endl;
+    for (int i = 0; i < 4; ++i)
+        cout << stack1.pop() << endl;
+
+    Queue<int> queue1;
+    queue1.en_queue(5);
+    queue1.en_queue(7);
+    queue1.en_queue(8);
+    cout << "Queue test: " << endl;
+    for (int i = 0; i < 3; ++i)
+        cout << queue1.de_queue() << endl;
 }
